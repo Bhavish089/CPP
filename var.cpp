cf43d47c84507b3b9594460496970d64f800e6c9
Bhavish089 <72312465+Bhavish089@users.noreply.github.com>
2025-01-14 12:06:37 +0530

    var

---
+++ var.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int x = 5;
+    float a = 6.7f;
+    const char* y = "Hello";
+    std::string z = "World";
+    std::cout << "int x = " << x;
+    std::cout << "\nfloat a = " << a;
+    std::cout << "\nchar y = " << y;
+    std::cout << "\nString z = " << z << std::endl;
+    return 0;
+}
