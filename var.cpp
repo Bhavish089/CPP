02712209a7bbac28a74ada63ec4bf47fa205b38c
Bhavish089 <72312465+Bhavish089@users.noreply.github.com>
2025-02-09 11:16:24 +0530

    variables and datatypes

--- var.cpp
+++ var.cpp
@@ -9,7 +9,11 @@ int main() {
     int arr[2] = {0,1};
 
     std::cout << "x data type: " << typeid(x).name() << std::endl;
+    std::cout << "int  =  i" << std::endl;
     std::cout << "y data type: " << typeid(y).name() << std::endl;
+    std::cout << "char = c" << std::endl;
     std::cout << "z data type: " << typeid(z).name() << std::endl;
+    std::cout << "float = f" << std::endl;
     std::cout << "arr data type: " << typeid(arr).name() << std::endl;
+    std::cout << "array = a2_i" << std::endl;
 }
